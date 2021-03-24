b9cca4fb69f3356a6e03ff2a65f33816692d657b
OmarAhmed3012 <omaraboshamaa@gmail.com>
2021-03-24 18:38:00 +0200

    Navbar

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,14 @@
 import Head from 'next/head'
+import Navbar from '../components/navbar'
 
 export default function Home() {
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Todo App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <Navbar></Navbar>
       <main>
         <h1>hey</h1>
       </main>
